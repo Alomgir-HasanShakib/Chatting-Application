beacf0c7e53662d83a9820947a677178c137a3af
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-12 00:14:45 +0600

    created auth controller and implement signup functionality into auth controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,10 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT | 3000;
-
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
 });
-
