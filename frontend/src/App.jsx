85954dd8d647186cf02cce3877fc33923dc03d27
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-13 17:29:17 +0600

    created settings page and implemented theme switch functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,12 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { checkAuth, authUser, isCheckingAuth } = useAuthStore();
+
+  const {theme} = useThemeStore()
 
   useEffect(() => {
     checkAuth();
@@ -29,7 +32,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
